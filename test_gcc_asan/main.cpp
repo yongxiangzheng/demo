d461223806bc9ad19fbb5baf736462bbbd822e8e
root <root@DESKTOP-DET2EE0.localdomain>
2021-09-24 14:04:25 +0800

    编译选项增加-fsanitize=address -fno-omit-frame-pointer -g -O2，则在运行时，调用fun0会产生异 常，否则不会。

--- test_gcc_asan/main.cpp
+++ test_gcc_asan/main.cpp
@@ -1,8 +1,15 @@
 #include <QCoreApplication>
 
+// 内存越界
+int fun0(){
+    char str[4] = {0,};
+    strcpy(str,"测试");
+    return 0;
+}
+
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
-
+    fun0();
     return a.exec();
 }
